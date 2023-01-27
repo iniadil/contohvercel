703456d117fb272ffc7d44de6548087fac412224
Adil Muhammad <ivgcomunity@gmail.com>
2023-01-27 11:33:08 +0700

    update

---
+++ firebase/fireConfig.js
@@ -0,0 +1,14 @@
+export const fireConfig = {
+  type: "service_account",
+  project_id: "sitna-forum",
+  private_key_id: "3b52bdb58768f306a86e20e0826be78bea8ad7db",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCTcBMRxAfbOLlV\nnw1kMofhPO1ZyqT7EDmhf4ArpENWNogWkCKgNaJCj4YKdggUEVBRRYERWYdG7AhH\nAOmQTXEacQPkVRQfX4Pwfyv1JogA1NROk3GOS5bQcbh8FtELH9zYTXwWBMwYm5e6\n8x/InY34XxhsBac5SHewrNvzvQOcsj8NKsRbaJZv54ypqgXnJqPEBC6KBVQd4RNi\n61BEgZQGmrxZiPZuUqXdssBwmFKv4VR0XuiA8fJPAyVpv60CqIEOrJeVzqhdtpUq\nsJgcewM3jUqrbMsmVbPyu1erzwkh8jZrmR4BL6uG5e8hlPYevErIUQ8S/OjW2KhG\nScdh7UttAgMBAAECggEAA+Ax81YB46nNq9bu7sldRRCC9Mjpv8Ft1ZvMB/NDfRd/\nfSH0DXe1EkB2BGRh7GHs7vxl9JZnHF6SdFT6NTuz7QwET1/lLYX0EMIAqwS34oqC\niKZB//AAwO4CxlBOb0pt72NQdb7rVpV26AhoHeMpmw106OU+v7fyw+3Nwme5M2J5\n7wpUQJzOgVLdipnFJIDumOq1WCv6Bpl/8dP4XnXOsM3sawcQk7EaHf+JFn47LbBA\nTRs8kspDn3K7M+kRdLZEzoekIoFxL2FctBBBLcZb5BXROfm4TRhyP5rdAeOmCyw2\nw35Esp4jhbRCVpUW1R6E51r2DGmLmUT+3xJn59s+yQKBgQDKqdcXjr9COkvhLrso\n5DZIjcoD1SlexwPaRDmsKXfnV7zDndbKCGtSV7QDzCswy8ckmBrhuwF6EYFY1Lnc\np+hrbJ1olbRAIqbtj0WL5Pvrpqjzn9q48VKKUzVlm78cWPnHKSTBqnl0C2Dpn/DA\nxDE8+nseoujx9sVKMut0++q5dwKBgQC6PXwysUgKfJx+B6p8+m2bbW31Qnt1/yUA\nwOnH4gKrC/mMLT1+55X73fSmOti0av4iOcVpUVDvk0h2qHpoIffosrHix2cRoEMs\n1Xc565psfxqXJnlsMbEOoXA4FoC/m+DJpihAY9eZo2Ap4Fod2Dt2fkSkZANR7qsm\nycXG+iMbOwKBgQCjw8aZtLH8dFsopbh7fJ4Y1uqtWizJ94u3bnYkFoZbnNxePkd4\njoEa0uAWSX+FOMW9XmQqkBDLcSXSgGOIWO5JEEXKY156jQXS5XlPaEPh0pSn+T6n\nP+pw+HgnmUk/QO7l7utSy8zE5wTzBFlpi6A2JDolMYOHMt+N68O0jnTIVQKBgQCZ\nGYBm38vcKxIunLivYuMF2ZCCBTQfDS7rDqtgUNBmskJj1LDwCQsH8S90E34f2rCt\njBZX/KvvNoFkYh22Su5UUcngJpH5MGjoi1cmuyLIPDistsksgzENWPM6eG1AlrDU\ntA302N7dAyxDYJZUYHSJdUWSalE8Ylo6WbaHWwvPDwKBgQCBg1i+Tj8vhOSVp0R8\nb4PIjCJOF/GK68kiqjefrGrBhohv0jNKrnOu+Uzt4RAxrltUcZ0iEEuhAwGJEBEn\nRb297f4wyY0GtU64Y0AsZ+FYAaDY8poItXVsoL5T0/S9yZXSBmAnbJ1DwSo//uKM\nfKA5R/z/BkhVXwAbauoWIoUGgw==\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-zok81@sitna-forum.iam.gserviceaccount.com",
+  client_id: "105962041270562177291",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zok81%40sitna-forum.iam.gserviceaccount.com",
+};
